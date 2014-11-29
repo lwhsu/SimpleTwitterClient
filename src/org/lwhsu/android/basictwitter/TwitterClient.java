cc2ed93657cc939a75854e45a92ef0afa16bd58f
Li-Wen Hsu <lwhsu@lwhsu.org>
2014-11-30 00:57:29 +0800

    Finalize, organize code

--- src/org/lwhsu/android/basictwitter/TwitterClient.java
+++ src/org/lwhsu/android/basictwitter/TwitterClient.java
@@ -10,16 +10,16 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 /*
- * 
- * This is the object responsible for communicating with a REST API. 
+ *
+ * This is the object responsible for communicating with a REST API.
  * Specify the constants below to change the API being communicated with.
- * See a full list of supported API classes: 
+ * See a full list of supported API classes:
  *   https://github.com/fernandezpablo85/scribe-java/tree/master/src/main/java/org/scribe/builder/api
  * Key and Secret are provided by the developer site for the given API i.e dev.twitter.com
  * Add methods for each relevant endpoint in the API.
- * 
+ *
  * NOTE: You may want to rename this object based on the service i.e TwitterClient or FlickrClient
- * 
+ *
  */
 public class TwitterClient extends OAuthBaseClient {
 	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change this
@@ -28,16 +28,16 @@ public class TwitterClient extends OAuthBaseClient {
 	public static final String REST_CONSUMER_SECRET = "SOME_SECRET"; // Change this
 	public static final String REST_CALLBACK_URL = "oauth://cprest"; // Change this (here and in manifest)
 
-	public TwitterClient(Context context) {
+	public TwitterClient(final Context context) {
 		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
 	}
 
 	// CHANGE THIS
 	// DEFINE METHODS for different API endpoints here
-	public void getInterestingnessList(AsyncHttpResponseHandler handler) {
-		String apiUrl = getApiUrl("?nojsoncallback=1&method=flickr.interestingness.getList");
+	public void getInterestingnessList(final AsyncHttpResponseHandler handler) {
+		final String apiUrl = getApiUrl("?nojsoncallback=1&method=flickr.interestingness.getList");
 		// Can specify query string params directly or through RequestParams.
-		RequestParams params = new RequestParams();
+		final RequestParams params = new RequestParams();
 		params.put("format", "json");
 		client.get(apiUrl, params, handler);
 	}
