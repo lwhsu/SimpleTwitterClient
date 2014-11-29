b36305239c86c447482f65b4bc08ea02f63b3721
Li-Wen Hsu <lwhsu@lwhsu.org>
2014-11-30 02:33:26 +0800

    Store Twitter key/secert

--- src/org/lwhsu/android/basictwitter/TwitterClient.java
+++ src/org/lwhsu/android/basictwitter/TwitterClient.java
@@ -24,8 +24,8 @@ import com.loopj.android.http.RequestParams;
 public class TwitterClient extends OAuthBaseClient {
 	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change this
 	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = "SOME_KEY";       // Change this
-	public static final String REST_CONSUMER_SECRET = "SOME_SECRET"; // Change this
+	public static final String REST_CONSUMER_KEY = TwitterConfig.TWITTER_API_KEY;
+	public static final String REST_CONSUMER_SECRET = TwitterConfig.TWITTER_API_SECRET;
 	public static final String REST_CALLBACK_URL = "oauth://cprest"; // Change this (here and in manifest)
 
 	public TwitterClient(final Context context) {
