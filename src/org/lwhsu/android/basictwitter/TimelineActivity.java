ce618a66f19fcab1efb7b9a40352c9316537ade4
Li-Wen Hsu <lwhsu@lwhsu.org>
2014-11-30 05:14:08 +0800

    Load tweets into listview

--- src/org/lwhsu/android/basictwitter/TimelineActivity.java
+++ src/org/lwhsu/android/basictwitter/TimelineActivity.java
@@ -1,16 +1,24 @@
 package org.lwhsu.android.basictwitter;
 
+import java.util.ArrayList;
+
 import org.json.JSONArray;
+import org.lwhsu.android.basictwitter.models.Tweet;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class TimelineActivity extends Activity {
 
     private TwitterClient client;
+    private ArrayList<Tweet> tweets;
+    private ArrayAdapter<Tweet> aTweets;
+    private ListView lvTweets;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -18,13 +26,17 @@ public class TimelineActivity extends Activity {
         setContentView(R.layout.activity_timeline);
         client = TwitterApplication.getRestClient();
         populateTimeline();
+        lvTweets = (ListView) findViewById(R.id.lvTweets);
+        tweets = new ArrayList<Tweet>();
+        aTweets = new ArrayAdapter<Tweet>(this, android.R.layout.simple_list_item_1, tweets);
+        lvTweets.setAdapter(aTweets);
     }
 
     public void populateTimeline() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(final JSONArray json) {
-                Log.d("debug", json.toString());
+                aTweets.addAll(Tweet.fromJSONArray(json));
             }
 
             @Override
--- src/org/lwhsu/android/basictwitter/models/Tweet.java
+++ src/org/lwhsu/android/basictwitter/models/Tweet.java
@@ -1,7 +1,12 @@
 package org.lwhsu.android.basictwitter.models;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.util.Log;
 
 public class Tweet {
     private String body;
@@ -14,10 +19,11 @@ public class Tweet {
         try {
             tweet.body = json.getString("text");
             tweet.uid = json.getLong("id");
-            tweet.createdAt = json.getString("created_at;");
+            tweet.createdAt = json.getString("created_at");
             tweet.user = User.fromJSON(json.getJSONObject("user"));
         } catch (final JSONException e) {
             e.printStackTrace();
+            Log.d("error", "error1");
             return null;
         }
         return tweet;
@@ -37,6 +43,32 @@ public class Tweet {
 
     public User getUser() {
         return user;
+    }
+
+    public static ArrayList<Tweet> fromJSONArray(final JSONArray json) {
+        final ArrayList<Tweet> tweets = new ArrayList<Tweet>(json.length());
+
+        for (int i = 0; i < json.length(); i++) {
+            JSONObject tweetJson = null;
+            try {
+                tweetJson = json.getJSONObject(i);
+            } catch (final Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+
+            final Tweet tweet = Tweet.fromJSON(tweetJson);
+            if (tweet != null) {
+                tweets.add(tweet);
+            }
+        }
+
+        return tweets;
+    }
+
+    @Override
+    public String toString() {
+        return getBody() + " - " + getUser().getScreenName();
     }
 
 }
