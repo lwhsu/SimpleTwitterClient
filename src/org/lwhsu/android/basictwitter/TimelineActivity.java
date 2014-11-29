ea30e7d67e509e41511be31385625b81be344d37
Li-Wen Hsu <lwhsu@lwhsu.org>
2014-11-30 04:04:45 +0800

    Cleanup and finalize

--- src/org/lwhsu/android/basictwitter/TimelineActivity.java
+++ src/org/lwhsu/android/basictwitter/TimelineActivity.java
@@ -2,13 +2,11 @@ package org.lwhsu.android.basictwitter;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class TimelineActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
     }
