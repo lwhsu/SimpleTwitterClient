9864820f2a753a86d4ea1bb77cc1bbbadb2658ee
Li-Wen Hsu <lwhsu@lwhsu.org>
2014-12-08 00:21:13 +0800

    Move everything about home timeline to HomeTimelineFragment

--- src/org/lwhsu/android/basictwitter/TimelineActivity.java
+++ src/org/lwhsu/android/basictwitter/TimelineActivity.java
@@ -1,89 +1,19 @@
 package org.lwhsu.android.basictwitter;
 
-import java.util.ArrayList;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.lwhsu.android.basictwitter.fragments.TweetsListFragment;
-import org.lwhsu.android.basictwitter.models.Tweet;
-import org.lwhsu.android.basictwitter.models.User;
-
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.activeandroid.query.Delete;
-import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class TimelineActivity extends FragmentActivity {
-
-    private TwitterClient client;
-    private TweetsListFragment fragementTweetsList;
-
-    private long lastId;
-
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-        client = TwitterApplication.getRestClient();
-        fragementTweetsList = (TweetsListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_timeline);
-        if (isConnected()) {
-            populateTimeline(Long.valueOf(1), null);
-        } else {
-            Toast.makeText(this, "No Network Connection!", Toast.LENGTH_LONG).show();
-            populateTimelineOffline();
-        }
-        /*
-        lvTweets.setOnScrollListener(new EndlessScrollListener() {
-
-            @Override
-            public void onLoadMore(final int page, final int totalItemsCount) {
-                populateTimeline(null, Long.valueOf(lastId) - 1);
-            }
-
-        });
-        */
-    }
-
-    public void populateTimeline(final Long sinceId, final Long maxId) {
-        client.getHomeTimeline(sinceId, maxId, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(final JSONArray json) {
-                final ArrayList<Tweet> tweets = Tweet.fromJSONArray(json);
-                fragementTweetsList.addAll(tweets);
-                lastId = tweets.get(tweets.size() - 1).getUid();
-
-                // update local db
-                new Delete().from(Tweet.class).execute();
-                new Delete().from(User.class).execute();
-                for (final Tweet tweet : tweets) {
-                    tweet.getUser().save();
-                    tweet.save();
-                }
-            }
-
-            @Override
-            public void onFailure(final Throwable e, final String s) {
-                Log.d("debug", e.toString());
-                Log.d("debug", s.toString());
-            }
-        });
-    }
-
-    private void populateTimelineOffline() {
-        fragementTweetsList.addAll((ArrayList<Tweet>) Tweet.getAll());
-    }
-
-    public void prependTimeline(final Tweet tweet) {
-        fragementTweetsList.prependTimeLine(tweet);
     }
 
     @Override
@@ -105,6 +35,7 @@ public class TimelineActivity extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /*
     @Override
     protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
         if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_COMPOSE) {
@@ -124,6 +55,7 @@ public class TimelineActivity extends FragmentActivity {
             });
         }
     }
+    */
 
     private boolean isConnected() {
         final ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
---
+++ src/org/lwhsu/android/basictwitter/fragments/HomeTimelineFragment.java
@@ -0,0 +1,80 @@
+package org.lwhsu.android.basictwitter.fragments;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.lwhsu.android.basictwitter.TwitterApplication;
+import org.lwhsu.android.basictwitter.TwitterClient;
+import org.lwhsu.android.basictwitter.models.Tweet;
+import org.lwhsu.android.basictwitter.models.User;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.activeandroid.query.Delete;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+public class HomeTimelineFragment extends TweetsListFragment {
+
+    private TwitterClient client;
+    private long lastId;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        client = TwitterApplication.getRestClient();
+
+        //if (isConnected()) {
+            populateTimeline(Long.valueOf(1), null);
+        /*
+        } else {
+            Toast.makeText(this, "No Network Connection!", Toast.LENGTH_LONG).show();
+            populateTimelineOffline();
+        }
+        */
+        /*
+        lvTweets.setOnScrollListener(new EndlessScrollListener() {
+
+            @Override
+            public void onLoadMore(final int page, final int totalItemsCount) {
+                populateTimeline(null, Long.valueOf(lastId) - 1);
+            }
+
+        });
+        */
+    }
+
+    public void populateTimeline(final Long sinceId, final Long maxId) {
+        client.getHomeTimeline(sinceId, maxId, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(final JSONArray json) {
+                final ArrayList<Tweet> tweets = Tweet.fromJSONArray(json);
+                addAll(tweets);
+                lastId = tweets.get(tweets.size() - 1).getUid();
+
+                // update local db
+                new Delete().from(Tweet.class).execute();
+                new Delete().from(User.class).execute();
+                for (final Tweet tweet : tweets) {
+                    tweet.getUser().save();
+                    tweet.save();
+                }
+            }
+
+            @Override
+            public void onFailure(final Throwable e, final String s) {
+                Log.d("debug", e.toString());
+                Log.d("debug", s.toString());
+            }
+        });
+    }
+
+    private void populateTimelineOffline() {
+        addAll((ArrayList<Tweet>) Tweet.getAll());
+    }
+
+    public void prependTimeline(final Tweet tweet) {
+        prependTimeLine(tweet);
+    }
+
+}
